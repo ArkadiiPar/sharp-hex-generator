22bc1fbc564c17c1b60423a39dcd42b4577a46bd
ArkadiiPar <31223025+ArkadiiPar@users.noreply.github.com>
2025-05-02 22:30:03 +0300

    Update hex_generator_web.py

--- hex_generator_web.py
+++ hex_generator_web.py
@@ -114,27 +114,13 @@ def generate_sharp_hex(values_list, level_names, level_slices, start_header=True
 
 # --- LUMA HEX Data ---
 luma_original_blocks = [
-    # Bayer luma denoise very low
     ["0000803f", "15", "cdcccc3d", "1d", "ae50223f", "0a0f0d"],
-    
-    # Bayer luma denoise low
     ["6666663f", "15", "cdcccc3d", "1d", "95806d3e", "0a0f0d"],
-    
-    # Bayer luma denoise med
     ["9a99593f", "15", "cdcc4c3d", "1d", "09997a3e", "0a0f0d"],
-    
-    # Bayer luma denoise high
     ["cdcc4c3f", "15", "cdcc4c3d", "1d", "0e06743e", "0a0a0d"],
-    
-    # Bayer luma denoise very high
     ["0000403f", "1d", "68ceb13e", "12050d0000a0401dcdcccc3f250000003f0a610a0f0d"],
-
-    # Sharp bento low
     ["cdcc4c3f", "15", "cdcccc3d", "1d", "65a5113f", "0a0f0d"],
-    
-    # Sharp bento high
     ["3333333f", "15", "cdcccc3d", "1d", "5a469a3e", "0a0f0d"],
-    
     ["3333333f", "15", "9a99993d", "1d", "6616913e", "0a0f0d"],
     ["9a99193f", "15", "0000803d", "1d", "f20bbf3e", "0a0a0d"],
     ["0000003f", "1d", "d1ff143f", "12050d0000a0411dcdcccc3f250000003f0a610a0f0d"],
@@ -157,10 +143,7 @@ luma_level_positions = {
     "Bayer luma denoise low": 1,
     "Bayer luma denoise med": 2,
     "Bayer luma denoise high": 3,
-    "Bayer luma denoise very high": 4,
-
-    "Sharp bento low": 5,
-    "Sharp bento high": 6
+    "Bayer luma denoise very high": 4
 }
 
 
@@ -179,47 +162,46 @@ def generate_luma_hex(values_list, block_templates, level_positions):
     lines = []
 
     for i, values in enumerate(values_list):
-        l1, l1a, l1b, l2, l2a, l2b, l3, l3a, l3b, l4, l4a, l4b, l5, l5a = values
-        level_name = values_list[i]["name"] if isinstance(values_list[i], dict) else values_list[i][0]
-        if isinstance(level_name, dict):
-            level_name = level_name["name"]
-
-        block_index = luma_level_positions[level_name]
+        # Получаем имя уровня из luma_levels
+        level_name = luma_levels[i]["name"]
+        
+        # Получаем шаблон блока
+        block_index = level_positions[level_name]
         modified_block = block_templates[block_index].copy()
 
         # Замена значений по индексам внутри блока
         if len(modified_block) > 0:
-            modified_block[0] = float_to_hex(l1)
+            modified_block[0] = float_to_hex(values[0])   # L1
         if len(modified_block) > 2:
-            modified_block[2] = float_to_hex(l1a)
+            modified_block[2] = float_to_hex(values[1])  # L1A
         if len(modified_block) > 4:
-            modified_block[4] = float_to_hex(l1b)
+            modified_block[4] = float_to_hex(values[2])  # L1B
 
         if len(modified_block) > 6:
-            modified_block[6] = float_to_hex(l2)
+            modified_block[6] = float_to_hex(values[3])  # L2
         if len(modified_block) > 8:
-            modified_block[8] = float_to_hex(l2a)
+            modified_block[8] = float_to_hex(values[4])  # L2A
         if len(modified_block) > 10:
-            modified_block[10] = float_to_hex(l2b)
+            modified_block[10] = float_to_hex(values[5]) # L2B
 
         if len(modified_block) > 12:
-            modified_block[12] = float_to_hex(l3)
+            modified_block[12] = float_to_hex(values[6])  # L3
         if len(modified_block) > 14:
-            modified_block[14] = float_to_hex(l3a)
+            modified_block[14] = float_to_hex(values[7]) # L3A
         if len(modified_block) > 16:
-            modified_block[16] = float_to_hex(l3b)
+            modified_block[16] = float_to_hex(values[8]) # L3B
 
         if len(modified_block) > 18:
-            modified_block[18] = float_to_hex(l4)
+            modified_block[18] = float_to_hex(values[9])  # L4
         if len(modified_block) > 20:
-            modified_block[20] = float_to_hex(l4a)
+            modified_block[20] = float_to_hex(values[10]) # L4A
         if len(modified_block) > 22:
-            modified_block[22] = float_to_hex(l4b)
+            modified_block[22] = float_to_hex(values[11]) # L4B
 
         if len(modified_block) > 24:
-            modified_block[24] = float_to_hex(l5)
+            modified_block[24] = float_to_hex(values[12]) # L5
         if len(modified_block) > 26:
-            modified_block[26] = float_to_hex(l5a)
+            modified_block[26] = float_to_hex(values[13]) # L5A
 
         lines.extend(modified_block)
 
